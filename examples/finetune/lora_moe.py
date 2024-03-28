321cdce75a3da9f15cf878ebc93a71dd0ce22d54
lzhangbv <lzhangbv@connect.ust.hk>
2024-03-28 17:31:33 +0800

    add: load balancing loss for mixtral-moe training

--- examples/finetune/lora_moe.py
+++ examples/finetune/lora_moe.py
@@ -6,17 +6,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers.activations import ACT2FN
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_id = "mistralai/Mistral-7B-v0.1"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-model = AutoModelForCausalLM.from_pretrained(model_id)
-model.eval()
 
 LORA_R = 32
 TOP_K = 2
 NUM_EXPERTS = 8
+AUX_LOSS_COEF = 0.02
+
+
+def load_balancing_loss_func(
+    routing_weights, expert_mask, num_experts, loss_coef):
+    """
+    Computes auxiliary load balancing loss as in Switch Transformer.
+    """
+    # Compute the percentage of tokens routed to each experts
+    tokens_per_expert = torch.mean(expert_mask.float(), dim=0)
+
+    # Compute the average probability of routing to these experts
+    router_prob_per_expert = torch.mean(routing_weights, dim=0)
+
+    aux_loss = torch.sum(tokens_per_expert * router_prob_per_expert.unsqueeze(0))
+    aux_loss = aux_loss * num_experts * loss_coef
+    return aux_loss
+
+
+class MoEAuxLoss(torch.autograd.Function):
+    """Compute the grad for auxiliary loss"""
+    @staticmethod
+    def forward(ctx, output: torch.Tensor, aux_loss: torch.Tensor):
+        ctx.save_for_backward(aux_loss)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        (aux_loss,) = ctx.saved_tensors
+        scaled_aux_loss_grad = torch.ones_like(aux_loss)
+        return grad_output, scaled_aux_loss_grad
+
 
 class MixtralBLockSparseTop2MLPLora(nn.Module):
     def __init__(self, config, w1, w2, w3):
@@ -62,6 +88,7 @@ class MixtralSparseMoeBlockLora(nn.Module):
         self.ffn_dim = config.intermediate_size
         self.num_experts = NUM_EXPERTS
         self.top_k = TOP_K
+        self.router_aux_loss_coef = AUX_LOSS_COEF
 
         # gating
         self.gate = nn.Linear(self.hidden_dim, self.num_experts, bias=False)
@@ -76,11 +103,12 @@ class MixtralSparseMoeBlockLora(nn.Module):
         """ """
         batch_size, sequence_length, hidden_dim = hidden_states.shape
         hidden_states = hidden_states.view(-1, hidden_dim)
+        
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(hidden_states)
 
-        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
-        routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+        all_routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights, selected_experts = torch.topk(all_routing_weights, self.top_k, dim=-1)
         routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
         # we cast back to the input dtype
         routing_weights = routing_weights.to(hidden_states.dtype)
@@ -91,8 +119,15 @@ class MixtralSparseMoeBlockLora(nn.Module):
 
         # One hot encode the selected experts to create an expert mask
         # this will be used to easily index which expert is going to be sollicitated
-        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts)
 
+        # auxiliary loss
+        if self.router_aux_loss_coef > 0 and self.training: 
+            aux_loss = load_balancing_loss_func(all_routing_weights, expert_mask, self.num_experts, self.router_aux_loss_coef)
+            routing_weights = MoEAuxLoss.apply(routing_weights, aux_loss)
+            #print(f"aux loss: {aux_loss.item()}")
+
+        expert_mask = expert_mask.permute(2, 1, 0)
         # Loop over all available experts in the model and perform the computation on each expert
         for expert_idx in range(self.num_experts):
             expert_layer = self.experts[expert_idx]
@@ -117,21 +152,55 @@ class MixtralSparseMoeBlockLora(nn.Module):
         final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
         return final_hidden_states
 
-def _set_module(model, submodule_key, module):
-    tokens = submodule_key.split('.')
-    sub_tokens = tokens[:-1]
-    cur_mod = model
-    for s in sub_tokens:
-        cur_mod = getattr(cur_mod, s)
-    setattr(cur_mod, tokens[-1], module)
 
-for name, module in model.named_modules():
-    if "mlp" in name and "mlp." not in name:
-        print(name)
-        _set_module(model, name, MixtralSparseMoeBlockLora(model.config, module))
+def make_lora_moe(model):
+    # freeze base model
+    for name, p in model.named_parameters():
+        p.requires_grad = False
+    
+    # add router and lora adaptors
+    for name, module in model.named_modules():
+        if "mlp" in name and "mlp." not in name:
+            print(name)
+            moe_block_lora = MixtralSparseMoeBlockLora(model.config, module)
+            parent_name = name.rsplit('.', 1)[0]
+            parent = model.get_submodule(parent_name)
+            setattr(parent, name[len(parent_name) + 1:], moe_block_lora)
 
-# test
-text = "Given that f(x) = 4x^3 - 9x - 14, find the value of f(2). Based on this result, find x such that f(x) = 0."
-inputs = tokenizer(text, return_tensors="pt")
-outputs = model.generate(**inputs, max_new_tokens=1024)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+def report_memory(model_id):
+    config = AutoConfig.from_pretrained(model_id)
+    model = LlamaMLP(config)
+    model = MixtralSparseMoeBlockLora(config, model)
+
+    model.to(device='cuda')
+
+    batch_size, seq_len = 4, 2048
+    dim = config.hidden_size
+    x = torch.randn((batch_size, seq_len, dim), device='cuda')
+    
+    loss = model(x).mean()
+    loss.backward()
+    print(f"Memory used: {torch.cuda.max_memory_reserved() / 1e9:.02f} GB")
+
+
+if __name__ == '__main__':
+    from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+    from transformers.models.llama.modeling_llama import LlamaMLP
+    
+    model_id = "mistralai/Mistral-7B-v0.1"
+
+    #report_memory(model_id)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map='cuda')
+    model.eval()
+
+    make_lora_moe(model)
+    model.to(device='cuda', dtype=torch.float16)
+
+    text = "What is deep learning?"
+    inputs = tokenizer(text, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_new_tokens=128)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
